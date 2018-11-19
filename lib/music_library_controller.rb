50e071e47423bce95174ea170e485d5314d17f86
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-19 00:01:19 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,9 +6,9 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
   def call
+    input = ""
     message = ["Welcome to your music library!", "To list all of your songs, enter 'list songs'.", "To list all of the artists in your library, enter 'list artists'.", "To list all of the genres in your library, enter 'list genres'.", "To list all of the songs by a particular artist, enter 'list artist'.", "To list all of the songs of a particular genre, enter 'list genre'.", "To play a song, enter 'play song'.", "To quit, type 'exit'.", "What would you like to do?"]
     message.each{|phrase| puts phrase}
-    input = ""
     while input != "exit"
       puts "What would you like to do?"
       input = gets
